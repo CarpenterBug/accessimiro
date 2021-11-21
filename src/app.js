44fa10801f5ab0e74e2228547774aeed9286c1d9
MADPT <miguelftangelo@gmail.com>
2021-11-21 02:04:35 +0200

    Add textToSpeech

--- src/app.js
+++ src/app.js
@@ -59,138 +59,24 @@ async function init() {
         return response.text();
       })
       .then(function (data) {
-				console.log(data);
-				let tex = JSON.parse(data.replace(/'/g, '"'))['text'];
-				let responseDOMCont = document.querySelector("#response-cont");
-				responseDOMCont.innerHTML = "You said: " +tex;
-				setTimeout(() => {
-					responseDOMCont.innerHTML = "";
-				}, 5000);
+        console.log(data);
+        let tex = JSON.parse(data.replace(/'/g, '"'))["text"];
+        let responseDOMCont = document.querySelector("#response-cont");
+        responseDOMCont.innerHTML = "You said: " + tex;
+        setTimeout(() => {
+          responseDOMCont.innerHTML = "";
+        }, 5000);
         recBtn.classList.remove("button-loading");
         recBtn.removeAttribute("disabled");
+        textToSpeech(tex);
       });
   }
 
-  //   invokeSaveAsDialog(blob);
-  /*
-    var recorder;
-  navigator.mediaDevices
-    .getUserMedia({
-      audio: true,
-      video: false,
-    })
-    .then(async function (stream) {
-      recorder = RecordRTC(stream, {
-        type: "audio",
-        mimeType: "audio/wav",
-        recorderType: StereoAudioRecorder,
-        numberOfAudioChannels: 1,
-        ondataavailable: function (blob) {
-          console.log("blob", blob);
-
-          let formData = new FormData();
-          formData.append("file", blob, "audio.wav");
-
-          fetch("https://dgculturecommittee.me/submit", {
-            method: "POST",
-            cors: "no-cors",
-            body: form,
-          })
-            .then(function (response) {
-              return response.text();
-            })
-            .then(function (data) {
-              console.log(data);
-            });
-        },
-      });
-
-      let blob;
-      let recBtn = document.querySelector("#record-btn");
-
-      recBtn.addEventListener("click", () => {
-        if (recBtn.classList.contains("button-danger")) {
-          recorder.stopRecording(function () {
-            blob = recorder.getBlob();
-          });
-
-          recBtn.innerHTML = "Record Command";
-          recBtn.classList.remove("button-danger");
-        } else {
-          console.log("recording");
-
-          recBtn.innerHTML = "Recording";
-          recBtn.classList.add("button-danger");
-
-          recorder.startRecording();
-        }
-      });
-    });
-    */
-
-  /* ----- */
-
-  /*
-  var recorder;
-  let responseDOMCont = document.querySelector("#response-cont");
-
-  navigator.mediaDevices
-    .getUserMedia({ audio: true })
-    .then((stream) => {
-      // store streaming data chunks in array
-      const chunks = [];
-
-      // create media recorder instance to initialize recording
-      recorder = new MediaRecorder(stream);
-      // function to be called when data is received
-      recorder.ondataavailable = (e) => {
-        // add stream data to chunks
-        chunks.push(e.data);
-        // if recorder is 'inactive' then recording has finished
-        if (recorder.state == "inactive") {
-          console.log("recording finished");
-
-          const blob = new Blob(chunks, { type: "audio/webm" });
-          // send blob to server
-          //send audio to wit.ai
-          var data = new FormData();
-          data.append("audio", blob, "audio.wav");
-
-          fetch("https://dgculturecommittee.me/submit", {
-            method: "POST",
-            cors: "no-cors",
-            body: data,
-          })
-            .then(function (response) {
-              return response.text();
-            })
-            .then(function (data) {
-              console.log(data);
-            });
-
-          while (chunks.length > 0) chunks.pop();
-        }
-      };
-      let recBtn = document.querySelector("#record-btn");
-
-      recBtn.addEventListener("click", () => {
-        if (recBtn.classList.contains("button-danger")) {
-          recorder.stop();
-
-          recBtn.innerHTML = "Record Command";
-          recBtn.classList.remove("button-danger");
-        } else {
-          console.log("recording");
-
-          recBtn.innerHTML = "Recording";
-          recBtn.classList.add("button-danger");
-
-          recorder.start();
-        }
-      });
-    })
-    .catch(console.error);
-    */
+  function textToSpeech(text) {
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  }
 }
 
 init();
